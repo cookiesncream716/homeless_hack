34bd485070f1151993ad462e30147152fb9554e5
Heather Walker <cookiesncream@ladle.net>
2016-09-24 13:14:28 -0700

    package json

--- app/models/example.js
+++ app/models/example.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var uniqueValidator = require('mongoose-unique-validator')
+// var uniqueValidator = require('mongoose-unique-validator')
 
 var ExampleSchema = new mongoose.Schema({
 	name: {type: String, required: true, unique: true},
 	create_at: {type: Date, default: new Date}
 });
 
-ExampleSchema.plugin(uniqueValidator);
+// ExampleSchema.plugin(uniqueValidator);
 mongoose.model('Example', ExampleSchema);
